9aeabe419282697dc9e20a0dddfb87f0cc95eea6
Sprinter05 <sprinterhr@gmail.com>
2025-01-28 19:20:07 +0100

    init server commit

---
+++ src/server/connection.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"net"
+)
+
+func handleClient(c net.Conn) {
+
+}
--- src/server/main.go
+++ src/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	// Create a new server listening on the adress
+	l, err := net.Listen("tcp", "127.0.0.1:6969")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Endless loop to listen for connections
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go handleClient(c)
+	}
 }
