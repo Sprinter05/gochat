52d07ed0cf246089281c1e80b66cb1112f1f6c1c
Markel Mencia <markel.mnc@gmail.com>
2025-02-02 17:07:08 +0100

    Create client main method

---
+++ client/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+// Main method for the client
+
+func main() {
+	// Connects to the server
+	con, err := net.Dial("tcp4", "127.0.0.1:6969")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Closes conection once execution is over
+	defer con.Close()
+
+	// Starts listening for server packets
+	go Listen(con)
+	// Opens a shell
+	NewShell(con)
+}
