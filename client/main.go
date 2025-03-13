7612b9d17f29f672707a154a11e6ed1b7650cbab
Markel Mencia <markel.mnc@gmail.com>
2025-03-13 19:18:32 +0100

    Create .env functionalities to main function

--- client/main.go
+++ client/main.go
@@ -1,22 +1,43 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"log"
 	"net"
+	"os"
+
+	"github.com/joho/godotenv"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 // Main method for the client
-
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("error: not enough arguments")
+		return
+	}
+	// Gets .env pathname
+	err := godotenv.Load(os.Args[1])
+	if err != nil {
+		fmt.Println("error: invalid .env path")
+		return
+	}
+
 	// Connects to the server
-	con, err := net.Dial("tcp4", "127.0.0.1:6969")
+	socket := getSocket()
+	con, err := net.Dial("tcp4", socket)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// Closes conection once execution is over
 	defer con.Close()
+
+	// Initializes an empty user
+	CurUser = Client{}
+
+	// Opens the client SQLite database
 	dbpath, ok := os.LookupEnv("CLT_DB_PATH")
 	if !ok {
 		fmt.Print("error: variable CLT_DB_PATH not found\n")
@@ -25,8 +46,25 @@ func main() {
 	DB, _ = sql.Open("sqlite3", dbpath)
 	DeleteEntries() // TODO: remove this
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	pctReceived := make(chan struct{})
+
 	// Starts listening for server packets
 	go Listen(con, ctx, pctReceived)
 	// Opens a shell
 	NewShell(con, ctx, pctReceived)
+}
+
+func getSocket() string {
+	addr, ok := os.LookupEnv("SRV_ADDR")
+	if !ok {
+		fmt.Print("error: variable SRV_ADDR not found\n")
+	}
+	port, ok := os.LookupEnv("SRV_PORT")
+	if !ok {
+		fmt.Print("error: variable SRV_PORT not found\n")
+	}
+	return fmt.Sprintf("%s:%s", addr, port)
 }
