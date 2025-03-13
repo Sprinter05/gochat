6f0f3c67084e904b136b9f7195bff971814fc224
Markel Mencia <markel.mnc@gmail.com>
2025-03-13 19:20:08 +0100

    Fix commands.go commit

--- client/commands.go
+++ client/commands.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/Sprinter05/gochat/internal/spec"
 )
@@ -87,6 +88,147 @@ func Ver() error {
 func Help() error {
 	fmt.Println(helpText)
 
+	return nil
+}
+
+// Execution code of the VERBOSE command
+func Verbose() error {
+	IsVerbose = !IsVerbose
+	if IsVerbose {
+		fmt.Println("Verbose mode turned on")
+	} else {
+		fmt.Println("Verbose mode turned off")
+	}
+
+	return nil
+}
+
+// Prints the commands that are yet to receive a response
+func PrintPending() error {
+	// Checks the number of pending packets
+	if len(PendingBuffer) == 0 {
+		fmt.Printf("There are no pending packets\n")
+	} else {
+		fmt.Printf("Pending packets:\n----------------\n")
+		for v, i := range PendingBuffer {
+			fmt.Printf("ID %d: Action code: %d (%s)\n", v, i, spec.CodeToString(spec.Action(uint8(i))))
+		}
+	}
+
+	return nil
+}
+
+// Creates a user with a username received as input
+func CreateUser(cmd *spec.Command, nArg uint8) error {
+	// Checks argument count
+	if cmd.HD.Args != nArg {
+		return fmt.Errorf("%s: Incorrect number of arguments", spec.CodeToString(cmd.HD.Op))
+	}
+	user, createErr := NewUser(string(cmd.Args[0]))
+	if createErr != nil {
+
+		return createErr
+	}
+	fmt.Print("User " + user.username + " created successfully\n")
+	CurUser = *user
+
+	return nil
+}
+
+// Sends a REG package for the current user to the server automatically
+// TODO: Improve this
+func RegUser() error {
+	if (Client{}) == CurUser {
+		return fmt.Errorf("error: there is no current user logged in")
+	}
+
+	pubKey, keyErr := spec.PubkeytoPEM(&CurUser.keyPair.PublicKey)
+	if keyErr != nil {
+
+		return keyErr
+	}
+
+	args := [][]byte{[]byte(CurUser.username), pubKey}
+
+	payloadLen := 0
+	for _, arg := range args {
+		payloadLen += len(arg) + 2 // + 2 to include the CRLF in each argument
+	}
+
+	// Creates header
+	header := spec.Header{
+		Ver:  spec.ProtocolVersion,
+		Op:   spec.REG,
+		Info: spec.EmptyInfo,
+		Args: NumArgs["REGUSER"],
+		Len:  uint16(payloadLen),
+		ID:   spec.ID(spec.GeneratePacketID(PendingBuffer)),
+	}
+	// Creates command
+	cmd := spec.Command{HD: header, Args: args}
+	sendErr := SendPacket(&cmd, NumArgs["REGUSER"])
+	return sendErr
+}
+
+// Execution code of the MSG command (requires database insert and message encryption)
+func SendMSG(cmd *spec.Command, nArg uint8) error {
+	// Checks argument count
+	if cmd.HD.Args != nArg {
+		return fmt.Errorf("%s: Incorrect number of arguments", spec.CodeToString(cmd.HD.Op))
+	}
+	// Stores the message in plain text to be stored in the database later
+	plainMessage := cmd.Args[2]
+
+	// The packet message is taken and is encrypted
+	var encryptErr error
+
+	pem, _ := GetUserPubkey(string(cmd.Args[0]))
+	destPubKey, _ := spec.PEMToPubkey(pem)
+	cmd.Args[2], encryptErr = spec.EncryptText(cmd.Args[2], destPubKey)
+	if encryptErr != nil {
+		return encryptErr
+	}
+
+	// Packet is sent
+	sendErr := SendPacket(cmd, nArg)
+	if sendErr != nil {
+		return sendErr
+	}
+	// If the message is sent correctly, then it is also stored in the database
+	// Casts the received timestamp
+	stamp, _ := strconv.ParseInt(string(cmd.Args[1]), 10, 64)
+	destination_username := cmd.Args[0]
+	dbErr := AddMessage(CurUser.username, string(destination_username), stamp, string(plainMessage))
+
+	return dbErr
+}
+
+// Rearranges a packet to send a USRS packet
+func Usrs(cmd *spec.Command, nArg uint8) error {
+	// Checks argument count
+	if cmd.HD.Args != nArg {
+		return fmt.Errorf("%s: Incorrect number of arguments", spec.CodeToString(cmd.HD.Op))
+	}
+	var convErr error
+	// Moves the argument (which contains the USRS option) to the Info field of the header
+	infoVal, convErr := strconv.Atoi(string(cmd.Args[0]))
+	if convErr != nil {
+
+		return convErr
+	}
+	if !(infoVal == 0 || infoVal == 1) {
+
+		return fmt.Errorf("error: USRS argument should be 0 (all users) or 1 (online users)")
+	}
+	cmd.HD.Info = uint8(infoVal)
+	// Initializes the argument slice to remove arguments
+	cmd.Args = make([][]byte, 0)
+	// Sends the rearranged packet
+	sendErr := SendPacket(cmd, nArg)
+	if sendErr != nil {
+		return sendErr
+	}
+	printCmdIfVerbose(*cmd)
 	return nil
 }
 
