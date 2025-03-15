ad3fd09c3263b12e527ef76660567862e9e8b491
Markel Mencia <markel.mnc@gmail.com>
2025-03-15 18:16:21 +0100

    Fix NewUser return type

--- client/client.go
+++ client/client.go
@@ -12,26 +12,22 @@ type Client struct {
 	keyPair  *rsa.PrivateKey
 }
 
-// Creates a user given its username, generating a key pair for it
-// ! No devuelvas un pointer si en la funcion que lo usa lo vas a dereferenciar
-// ! Cada vez q mueves un pointer activas el garbage collector, asi que evitalo en la medida de lo posible
-// ! Solo deberias usar pointers si es para modificar algo
-func NewUser(username string) (*Client, error) {
+// Creates a user given its username, generating a key pair for it. The client data will be nil if an error occurs
+func NewUser(username string) (Client, error) {
 	// Generates a key pair
 	pair, genErr := rsa.GenerateKey(rand.Reader, spec.RSABitSize)
 	if genErr != nil {
-		return nil, genErr
+		return Client{username: "", keyPair: nil}, genErr
 	}
 	// Stores the pubkey's bytes to be stored in the database
 	pubKey, _ := spec.PubkeytoPEM(&pair.PublicKey)
-	// ! Evita comentarios superfluos que no aportan nada
-	// Creates the client
+
 	client := Client{username: username, keyPair: pair}
 	// Adds it to the database
 	dbErr := AddUser(username, string(pubKey))
 	if dbErr != nil {
-		return nil, dbErr
+		return Client{username: "", keyPair: nil}, dbErr
 	}
-	// Only if there are no errors is the client returned
-	return &client, nil
+
+	return client, nil
 }
