42d81be4565d277a37790d9e073e11db1c57fa61
Markel Mencia <markel.mnc@gmail.com>
2025-04-14 22:42:04 +0200

    Add core shell functionality

---
+++ client/shell.go
@@ -0,0 +1,55 @@
+package main
+
+// This package includes the core functionality of the gochat client shell
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+
+	"github.com/Sprinter05/gochat/internal/spec"
+)
+
+// Starts a shell that allows the client to send packets
+// to the gochat server, along with other functionalities.
+func NewShell(verbose bool) {
+	rd := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("gochat() > ")
+		// Reads user input
+		input, readErr := rd.ReadBytes('\n')
+		if readErr != nil {
+			fmt.Printf("input error: %s\n", readErr)
+			continue
+		}
+		// Trims the input, removing trailing spaces and line jumps
+		input = bytes.TrimSpace(input)
+		if len(input) == 0 {
+			// Empty command, asks for input again
+			continue
+		}
+
+		op := string(bytes.Fields(input)[0])
+		if op == "EXIT" {
+			return
+		}
+
+		var args [][]byte
+		args = append(args, bytes.Fields(input)[1:]...)
+
+		// TODO: NewPacket will be moved to each command function to fit the needs for each operation once they are ready
+		pct, pctErr := spec.NewPacket(spec.Action(spec.StringToCode(op)), 1, spec.EmptyInfo, args...) // TODO: ID assignation
+		if pctErr != nil {
+			fmt.Printf("packet creation error: %s\n", pctErr)
+			continue
+		}
+
+		if verbose {
+			cmd := spec.ParsePacket(pct)
+			if cmd.HD.Op != 0 {
+				cmd.Print()
+			}
+		}
+	}
+}
