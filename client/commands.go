f34a9234db022498cefb1a9a2b70f2c22e7b1a8a
Markel Mencia <markel.mnc@gmail.com>
2025-04-16 12:05:22 +0200

    Create VERBOSE command

--- client/commands.go
+++ client/commands.go
@@ -13,17 +13,28 @@ type CommandData struct {
 	// TODO: DB...
 }
 
-var clientCmds = map[string]func(data CommandData) error{
-	"VER": ver,
-	"REQ": req,
+var ClientCmds = map[string]func(data CommandData, verbose *bool) error{
+	"VER":     ver,
+	"VERBOSE": verbose,
+	"REQ":     req,
 }
 
-func ver(data CommandData) error {
+func ver(data CommandData, verbose *bool) error {
 	fmt.Printf("gochat version %d\n", spec.ProtocolVersion)
 	return nil
 }
 
-func req(data CommandData) error {
+func verbose(data CommandData, verbose *bool) error {
+	*verbose = !*verbose
+	if *verbose {
+		fmt.Println("verbose mode on")
+	} else {
+		fmt.Println("verbose mode off")
+	}
+	return nil
+}
+
+func req(data CommandData, verbose *bool) error {
 	pct, pctErr := spec.NewPacket(spec.REQ, 1, spec.EmptyInfo, data.Args...)
 	if pctErr != nil {
 		return pctErr
