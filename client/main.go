d8b658d07a98faa9c6faba5b912db76a3f8457a0
Markel Mencia <markel.mnc@gmail.com>
2025-04-16 12:23:19 +0200

    Add .env interaction

--- client/main.go
+++ client/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	NewShell(true)
+	if len(os.Args) < 2 {
+		log.Fatal("error: not enough arguments")
+		return
+	}
+
+	// Gets .env pathname
+	err := godotenv.Load(os.Args[1])
+	if err != nil {
+		log.Fatal("error: invalid .env path")
+		return
+	}
+
 }
