c1b0b67b8fcc26b6c817addfcdbc68d5a9340828
Markel Mencía <markel.mnc@gmail.com>
2025-04-21 13:26:29 +0200

    Fix User-Server reference

--- client/database.go
+++ client/database.go
@@ -6,29 +6,29 @@ import (
 
 // Generic user table that defines the columns every user shares
 type User struct {
-	ID       uint   `gorm:"primaryKey;autoIncrement;not null"`
-	Username string `gorm:"unique;not null"`
-	ServerID uint   `gorm:"primaryKey;not null"`
-	Server   Server `gorm:"foreignKey:ServerID;OnDelete:CASCADE"`
+	UserID   uint   `gorm:"autoIncrement:false;not null"`
+	ServerID uint   `gorm:"primaryKey;autoIncrement:false;not null"`
+	Username string `gorm:"primaryKey;not null"`
+	Server   Server `gorm:"foreignKey:ServerID;references:ServerID;constraint:OnDelete:CASCADE"`
 }
 
 // User extension dedicated to shell-created users.
 // The passwords should be hashed and the private
 // keys need to be stored
 type LocalUserData struct {
-	ID       uint   `gorm:"primaryKey;not null"`
+	UserID   uint   `gorm:"primaryKey;not null"`
 	Password string `gorm:"not null"`
 	PrvKey   string
-	User     User `gorm:"foreignKey:ID;OnDelete:CASCADE"`
+	User     User `gorm:"foreignKey:UserID;OnDelete:CASCADE"`
 }
 
 // User extension dedicated to REQ'd users. Only
 // their public key is needed to encrypt messages
 // to them
 type ExternalUserData struct {
-	ID     uint   `gorm:"primaryKey;not null"`
+	UserID uint   `gorm:"primaryKey;not null"`
 	PubKey string `gorm:"not null"`
-	User   User   `gorm:"foreignKey:ID;OnDelete:CASCADE"`
+	User   User   `gorm:"foreignKey:UserID;OnDelete:CASCADE"`
 }
 
 // Holds message data
@@ -37,13 +37,13 @@ type Message struct {
 	DestinationID   uint
 	Stamp           time.Time `gorm:"primaryKey;not null"`
 	Text            string
-	SourceUser      User `gorm:"foreignKey:SourceID;OnDelete:RESTRICT"`
-	DestinationUser User `gorm:"foreignKey:DestinationID;OnDelete:RESTRICT"`
+	SourceUser      User `gorm:"foreignKey:SourceID;references:UserID;OnDelete:RESTRICT"`
+	DestinationUser User `gorm:"foreignKey:DestinationID;references:UserID;OnDelete:RESTRICT"`
 }
 
 // Server indentifier that allows a multi-server platform
 type Server struct {
-	ID      uint   `gorm:"primaryKey;autoIncrement;not null"`
-	Port    uint16 `gorm:"primaryKey;not null"`
-	Address string
+	Address  string `gorm:"primaryKey;autoIncrement:false;not null"`
+	Port     uint16 `gorm:"primaryKey;autoIncrement:false;not null"`
+	ServerID uint   `gorm:"autoIncrement:false;not null"`
 }
