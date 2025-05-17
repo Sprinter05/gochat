bfdfaf2341c08d47f0c117f2af7360b5fc0b0dbd
Markel Mencía <markel.mnc@gmail.com>
2025-05-17 10:37:39 +0200

    Add error handling

--- client/db/database.go
+++ client/db/database.go
@@ -302,13 +302,19 @@ func StoreMessage(db *gorm.DB, src User, dst User, text string, stamp time.Time)
 // Returns a slice with every message between two users in a range of time
 func GetUsersMessagesRange(db *gorm.DB, src User, dst User, init time.Time, end time.Time) ([]Message, error) {
 	var messages []Message
-	db.Where("stamp BETWEEN ? AND ?", init, end).Where("(source_id = ? AND destination_id = ?) OR (source_id = ? AND destination_id = ?)", src.UserID, dst.UserID, dst.UserID, src.UserID).Find(&messages)
+	result := db.Where("stamp BETWEEN ? AND ?", init, end).Where("(source_id = ? AND destination_id = ?) OR (source_id = ? AND destination_id = ?)", src.UserID, dst.UserID, dst.UserID, src.UserID).Find(&messages)
+	if result.Error != nil {
+		return nil, result.Error
+	}
 	return messages, nil
 }
 
 // Returns a slice with all messages between two users
 func GetAllUsersMessages(db *gorm.DB, src User, dst User) ([]Message, error) {
 	var messages []Message
-	db.Where("(source_id = ? AND destination_id = ?) OR (source_id = ? AND destination_id = ?)", src.UserID, dst.UserID, dst.UserID, src.UserID).Find(&messages)
+	result := db.Where("(source_id = ? AND destination_id = ?) OR (source_id = ? AND destination_id = ?)", src.UserID, dst.UserID, dst.UserID, src.UserID).Find(&messages)
+	if result.Error != nil {
+		return nil, result.Error
+	}
 	return messages, nil
 }
